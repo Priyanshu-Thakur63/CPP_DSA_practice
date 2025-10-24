4e73097c983dfc4911d35d17b39da5bb8f14d4ad
Priyanshu-Thakur63 <thatcoldcoffee17100@gmail.com>
2025-10-25 01:08:02 +0530

    adding file to 03_basics

---
+++ 03_Conditions/oddEven.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cout << "Enter an positive integer :";
+  cin >> n;
+  if(n > 0){
+    if(n%2 == 0){
+      cout << "The given number is Even";
+    }
+    
+    else{
+      cout << "The given number is Odd";
+    }
+  }
+
+  else{
+    cout << "Wrong input";
+  }
+}
