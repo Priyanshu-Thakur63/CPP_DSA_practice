4b7009b6509024d452e4661a2517730351e871cb
Priyanshu-Thakur63 <thatcoldcoffee17100@gmail.com>
2025-10-31 02:40:19 +0530

    adding file to 05_PatternPrinting

---
+++ 05_PatternPrinting/starTriangleFlipped.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cout << "Enter :";
+  cin >> n;
+
+  // M-1
+  for(int i=1 ; i<=n ; i++){
+    int m= n-i;
+    for(int j=1 ; j<=m ; j++){
+      cout<< " ";
+    }
+    for(int k=1 ; k<=i ; k++){
+        cout<< "*";
+      }
+    cout<< endl;
+  }
+
+  // M-2
+
+  return 0;
+}
