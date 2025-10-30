45965f0ed0f0cc9166f90b1a5d6c1a3eb26e71e8
Priyanshu-Thakur63 <thatcoldcoffee17100@gmail.com>
2025-10-31 02:53:33 +0530

    adding files

--- 05_PatternPrinting/starTriangleFlipped.cpp
+++ 05_PatternPrinting/starTriangleFlipped.cpp
@@ -7,18 +7,28 @@ int main(){
   cin >> n;
 
   // M-1
+  // for(int i=1 ; i<=n ; i++){
+  //   int m= n-i;
+  //   for(int j=1 ; j<=m ; j++){
+  //     cout<< " ";
+  //   }
+  //   for(int k=1 ; k<=i ; k++){
+  //       cout<< "*";
+  //     }
+  //   cout<< endl;
+  // }
+
+  // M-2
   for(int i=1 ; i<=n ; i++){
     int m= n-i;
     for(int j=1 ; j<=m ; j++){
       cout<< " ";
     }
-    for(int k=1 ; k<=i ; k++){
+    for(int k=m+1 ; k<=n ; k++){
         cout<< "*";
       }
     cout<< endl;
   }
-
-  // M-2
 
   return 0;
 }
