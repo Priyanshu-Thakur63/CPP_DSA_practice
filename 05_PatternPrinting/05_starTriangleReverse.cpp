e5726175c7e47ca0f6b9dac15f8782588d95bd96
Priyanshu-Thakur63 <thatcoldcoffee17100@gmail.com>
2025-11-05 01:50:13 +0530

    adding files to 05_PatternPrinting

--- 05_PatternPrinting/05_starTriangleReverse.cpp
+++ 05_PatternPrinting/05_starTriangleReverse.cpp
@@ -6,7 +6,10 @@ int main(){
   cout<< "Enter a number : ";
   cin >> n;
   for(int i=1 ; i<=n ; i++){
-    int a= 
-    for()
+    for(int j=1 ; j<=(n+1-i) ; j++){
+      cout<< "* ";
+    }
+    cout<< endl;
   }
+  return 0;
 }
