e2e28d2e5c383edb9c7f6b35a54aa14f6e20ab98
Priyanshu-Thakur63 <thatcoldcoffee17100@gmail.com>
2025-11-26 11:05:50 +0530

    adding exe files

---
+++ 05_PatternPrinting/pp2.cpp
@@ -0,0 +1,15 @@
+// Print the following pattern
+//  Input: n = 4
+//  Output:
+//  1 2 3 4
+//  1 2 3
+//  1 2
+//  1
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cout<< "Enter number of lines :";
+  cin>> n;
+  for(int i=1; i<=n;
